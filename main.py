ad26af051488c2693051e95059be3719de9ab518
elisttm <elisttm@gmail.com>
2025-02-11 17:35:54 -0500

    massive updates to server pages + updated graphics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os, asyncio, quart, hypercorn, sqlite3, time
-from quart import request, redirect, url_for, render_template, send_from_directory
+from quart import request, redirect, render_template, send_from_directory
 import servers as srv
 
 path = os.path.dirname(os.path.realpath(__file__))+'/' # stupid
@@ -31,75 +31,60 @@ async def _index():
     return await render_template('index.html', hits=hits)
 
 @app.route('/projects')
-async def _projects():
-    return await render_template('projects.html')
-
 @app.route('/eli')
-async def _sona():
-    return await render_template('eli.html')
+@app.route('/bot')
+@app.route('/pack')
+@app.route('/pages')
+async def _simple_page():
+    return await render_template(f'{request.path[1:]}.html')
 
 @app.route('/servers')
-async def _servers():
-    return await render_template('servers.html', time=srv.timestamp, curtime=int(time.time()))
-
 @app.route('/servers/extra')
-async def _servers_extra():
-    return await render_template('servers-extra.html', time=srv.timestamp, curtime=int(time.time()))
+async def servers():
+    return await render_template(f'servers{"-extra" if "extra" in request.path else ""}.html', servers=srv.servers, time=srv.timestamp, curtime=int(time.time()))
 
 @app.route('/gmod')
-async def _gmod():
-    return await render_template('servers/gmod.html', time=srv.timestamp)
-
 @app.route('/tf2')
-async def _tf2():
-    return await render_template('servers/tf2.html', time=srv.timestamp)
-
 @app.route('/mc')
-async def _minecraft():
-    return await render_template('servers/minecraft.html', time=srv.timestamp)
+async def _game_page():
+    return await render_template(f'servers/{request.path[1:]}.html', servers=srv.servers, time=srv.timestamp)
 
-@app.route('/bot')
-async def _sillybot():
-    return await render_template('bot.html')
-
-@app.route('/pack')
-async def _pack():
-    return await render_template('pack.html')
-
-@app.route('/pages')
-async def _pages():
-    return await render_template('pages.html')
-
-
-@app.route('/motd')
-async def _motd():
-    return await render_template('etc/motd.html')
+@app.route('/motd', defaults={'game': None})
+@app.route('/motd/<game>')
+async def _motd(game):
+    return await render_template('servers/motd.html', game=game, have_pages=srv.xtra.have_pages)
 
 @app.route('/connect/<server>')
-async def _connect(server):
-    if server not in srv.servers or srv.servers[server]["game"] not in srv.source_games:
+async def _server_connect(server):
+    if server not in srv.servers or srv.servers[server]["game"] not in srv.xtra.source_games:
         return quart.abort(404)
-    return redirect(f"steam://connect/{srv.ip}:{srv.servers[server]['ip'][1]}/chungus", code=302)
+    return redirect(f"steam://connect/{':'.join(map(str, srv.servers[server]['ip']))}/chungus", code=302)
 
+@app.route('/info/<server>')
+async def _server_info(server):
+    if server not in srv.servers:
+        return quart.abort(404)
+    info = srv.servers[server]
+    return await render_template('servers/info.html', game=server, info=info, time=srv.timestamp, xtra=srv.xtra)
 
 @app.route('/index.html')
-async def redirect_index():
-    return redirect(url_for('_index'), code=301)
+async def _redirect_index():
+    return redirect("/", code=301)
 
 @app.route('/trashbot')
 @app.route('/sillybot')
 @app.route('/elibot')
-async def redirect_sillybot():
-    return redirect(url_for('_sillybot'), code=301)
+async def _redirect_sillybot():
+    return redirect("/bot", code=301)
 
 @app.route('/sona')
 @app.route('/fursona')
-async def redirect_sona():
-    return redirect(url_for('_sona'), code=301)
+async def _redirect_sona():
+    return redirect("/eli", code=301)
 
 @app.route('/minecraft')
-async def redirect_minecraft():
-    return redirect(url_for('_minecraft'), code=301)
+async def _redirect_minecraft():
+    return redirect("/mc", code=301)
 
 @app.route('/homunculus')
 async def homunculus():
@@ -114,16 +99,15 @@ async def static_from_root():
 @app.errorhandler(404)
 @app.errorhandler(500)
 async def error_handler(error):
-    response = quart.Response(await render_template('etc/error.html', errors={
+    response = quart.Response(await render_template('error.html', errors={
         404: ["[404] page not found", "the url youre trying to access does not exist! you likely followed a dead link or typed something wrong"],
-        500: ["[500] internal server error", "somewhere along the way there was an error processing your request. if this keeps happening, please get in contact",],
+        500: ["[500] internal server error", "somewhere along the way there was an error processing your request. if this keeps happening, please get in contact asap!",],
     }, error=error,), error.code)
     response.headers.set("X-Robots-Tag", "noindex")
     return response
 
 hyperconfig = hypercorn.config.Config()
 hyperconfig.bind = ["0.0.0.0:7575"]
-app.jinja_env.cache = {}
 
 if __name__ == '__main__':
     asyncio.run(hypercorn.asyncio.serve(app, hyperconfig))
--- servers.py
+++ servers.py
@@ -13,6 +13,11 @@ servers = {
         "name": "eli sandbox server",
         "ip": (ip, 27017),
     },
+    "jazz": {
+        "game": "gmod",
+        "name": "eli jazztronauts",
+        "ip": (ip, 27041),
+    },
     "tf2": {
         "game": "tf2",
         "name": "eli tf2 server",
@@ -23,32 +28,78 @@ servers = {
         "name": "eli hldm server",
         "ip": (ip, 27013),
     },
-    
-    # extra
-    "jazz": {
-        "game": "gmod",
-        "name": "eli jazztronauts",
-        "ip": (ip, 27041),
-    },
     "sven": {
         "game": "sven",
-        "name": "eli sven server",
+        "name": "eli sven coop",
         "ip": (ip, 27040),
     },
-    "mc": {
-        "game": "minecraft",
-        "name": "eli smp server",
+    "smp": {
+        "game": "mc",
+        "name": "eli smp",
         "ip": "mc.elisttm.space",
     },
     "creative": {
-        "game": "minecraft",
+        "game": "mc",
         "name": "creative server",
         "ip": "creative.elisttm.space",
     }
 }
 
+class xtra:
+    source_games = ("gmod", "tf2", "hldm", "sven", "l4d2")
+    have_pages = ("gmod", "tf2", "mc")
+    
+    full_names = {
+        "gmod":  "Garry's Mod",
+        "tf2":   "Team Fortress 2",
+        "sven":  "Sven-Coop",
+        "hldm":  "Half-Life: Deathmatch",
+        "mc":    "Minecraft",
+    }
+    
+    def tf2_gamemode(map_name):
+        prefixes = {
+            "arena_": "Arena",
+            "ctf_":   "Capture the Flag",
+            "cp_":    "Control Point",
+            "koth_":  "King of the Hill",
+            "mvm_":   "Mann vs. Machine",
+            "pass_":  "PASS Time",
+            "plr_":   "Payload Race",
+            "pl_":    "Payload",
+            "pd_":    "Player Destruction",
+            "sd_":    "Special Delivery",
+            "vsh_":   "Vs. Saxton Hale",
+            "tow_":   "Tug of War",
+            "zi_":    "Zombie Infection",
+        }
+        for prefix, mode in prefixes.items():
+            if map_name.lower().startswith(prefix):
+                return mode
+        return "Team Fortress 2"
+
+
+class server_info(object):
+    max_players = 0
+    player_count = 0
+    player_list = []
+    subtitleA = ""
+    subtitleB = ""
+    map_name = ""
+    gamemode = ""
+    version = ""
+    game = ""
+    def __init__(self, player_count, max_players, player_list, map_name, gamemode, subtitleA, subtitleB):
+        self.player_count = player_count
+        self.max_players = max_players
+        self.player_list = player_list
+        self.map_name = map_name
+        self.gamemode = gamemode
+        self.subtitleA = subtitleA
+        self.subtitleB = subtitleB
+
+
 path = os.path.dirname(os.path.realpath(__file__))+'/' # still stupid
-source_games = ("gmod", "tf2", "hldm", "sven", "l4d2")
 timestamp = 0
 
 def seconds(sec:int):
@@ -62,26 +113,12 @@ def seconds(sec:int):
         x = f"{hr}h {min}m {sec}s"
     return x
 
-class server_info(object):
-    max_players = 0
-    player_count = 0
-    player_list = []
-    subtitle = ""
-    map_name = ""
-    def __init__(self, player_count, max_players, player_list, subtitle, map_name):
-        self.player_count = player_count
-        self.max_players = max_players
-        self.player_list = player_list
-        self.subtitle = subtitle
-        self.map_name = map_name
-
 def query_server(server):
     game = servers[server]["game"]
     ip = servers[server]["ip"]
     playerlist = []
     try:
-
-        if game in source_games:
+        if game in xtra.source_games:
             q = a2s.info(ip, 0.2)
             if q.player_count > 0:
                 for player in a2s.players(ip):
@@ -90,35 +127,44 @@ def query_server(server):
                         "score": player.score,
                         "time": seconds(round(player.duration))
                     })
-            map_name = (q.map_name[:16] + " ...") if len(q.map_name) > 16 else q.map_name
-            subtitle = (q.game[:24] + "...") if len(q.game) > 24 else q.game
-            return server_info(q.player_count, q.max_players, playerlist, subtitle, map_name)
+            if game == "tf2":
+                subtitleA = xtra.tf2_gamemode(q.map_name)
+            else:
+                subtitleA = (q.game[:24] + "...") if len(q.game) > 24 else q.game
+            subtitleB = (q.map_name[:18] + "...") if len(q.map_name) > 18 else q.map_name
+            return server_info(q.player_count, q.max_players, playerlist, q.map_name, q.game, subtitleA, subtitleB)
         
-        elif game == "minecraft":
+        elif game == "mc":
             q = mcstatus.JavaServer.lookup(ip, 0.2).status()
             if q.players.sample:
                 for player in q.players.sample:
                     playerlist.append({"name": player.name,})
-            return server_info(q.players.online, q.players.max, playerlist, ip, q.version.name)
+            return server_info(q.players.online, q.players.max, playerlist, None, None, ip, q.version.name)
+        
     except (TimeoutError, ConnectionRefusedError):
-        raise TimeoutError("server offline...")
+        raise TimeoutError("server offline")
     except Exception as e:
         print(f"EXCEPTION in query_server({server}): {e}")
         return None
-    
+
+verdana = ImageFont.truetype(f"{path}static/Verdana-Bold.ttf", 11)
+arial = ImageFont.truetype(f"{path}static/Arial.ttf", 10)
 
 async def draw_banners():
     global timestamp
     while True:
-        for server in servers:
-            game = servers[server]["game"]
+        for server in servers.copy():
+            servers[server]["query"] = None
             try:
                 query = query_server(server)
-                img = Image.open(f"{path}static/img/servers/template-{game}.gif")
+                if not query:
+                    continue
+                servers[server]["query"] = query
+                img = Image.open(f"{path}static/img/servers/template-{servers[server]["game"]}.gif")
                 draw = ImageDraw.Draw(img)
-                draw.text((160, 1), f"{query.player_count}/{query.max_players}", "white", ImageFont.truetype(f"{path}static/Verdana-Bold.ttf", 11))
-                draw.text((160, 15.5), query.map_name, "white", ImageFont.truetype(f"{path}static/Arial.ttf", 10))
-                draw.text((34, 15.5), query.subtitle, "white", ImageFont.truetype(f"{path}static/Arial.ttf", 10))
+                draw.text((162, 1), f"{query.player_count}/{query.max_players}", "white", verdana)
+                draw.text((35, 15.5), query.subtitleA, "white", arial)
+                draw.text((162, 15.5), query.subtitleB, "white", arial)
             except TimeoutError:
                 img = Image.open(f"{path}static/img/servers/template-offline.gif")
                 draw = ImageDraw.Draw(img)
@@ -126,7 +172,7 @@ async def draw_banners():
                 print(server, str(e))
                 img = Image.open(f"{path}static/img/servers/template-error.gif")
                 draw = ImageDraw.Draw(img)
-            draw.text((34, 1), servers[server]['name'], "white", ImageFont.truetype(f"{path}static/Verdana-Bold.ttf", 11))
+            draw.text((35, 1), servers[server]['name'], "white", verdana)
             img.save(f"{path}static/img/servers/banner-{server}.gif")
         timestamp = int(time.time())
         await asyncio.sleep(60)
